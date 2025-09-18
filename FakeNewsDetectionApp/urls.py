7e152f61d3849f1faf4e988923e2e84d5388a932
Nived s manoj <nivedsmanoj01@gmail.com>
2025-09-18 13:50:02 +0530

    second commit

--- FakeNewsDetectionApp/urls.py
+++ FakeNewsDetectionApp/urls.py
@@ -1,30 +1,17 @@
-"""
-URL configuration for FakeNewsDetection project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 
-from FakeNewsDetectionApp.views import Complaint, Feedback, LoginView, Reply, ViewUser
+from FakeNewsDetectionApp.views import *
 
 urlpatterns = [
     path('',LoginView.as_view(),name='Login'), 
     path('ViewUser',ViewUser.as_view(),name='ViewUser'), 
+    path('AcceptUser/<int:id>',AcceptUser.as_view(),name='AcceptUser'),
+    path('RejectUser/<int:id>',RejectUser.as_view(),name='RejectUser'),
     path('Complaint',Complaint.as_view(),name='Complaint'), 
     path('Reply',Reply.as_view(),name='Reply'), 
     path('Feedback',Feedback.as_view(),name='Feedback'), 
+    path('adminhome',AdminHome.as_view(),name='Adminhome'), 
     
     
 ]
--- FakeNewsDetectionApp/views.py
+++ FakeNewsDetectionApp/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 
-from FakeNewsDetection.FakeNewsDetectionApp.models import LoginTable
+from FakeNewsDetectionApp.models import *
 
 # Create your views here.
 class LoginView(View):
@@ -12,26 +12,37 @@ class LoginView(View):
         username = request.POST.get('username')
         password = request.POST.get('password')
         try:
-            obj = LoginTable.objects.get(username=username, password=password)
+            obj = LoginTable.objects.get(Username=username, Password=password)
             request.session['user_id'] = obj.id
-            if obj.user_type == 'admin':
-                return HttpResponse("<script>alert('Login successful');window.location='/admin_dash'</script>")
-            elif obj.user_type == 'teacher':
-                return HttpResponse("<script>alert('Login successful');window.location='/teacher_home'</script>")
-            elif obj.user_type == 'student':
-                return HttpResponse("<script>alert('Login successful');window.location='/student_home'</script>")
+            if obj.Type == 'admin':
+                return HttpResponse("<script>alert('Login successful');window.location='/adminhome'</script>")
         except LoginTable.DoesNotExist:
             return HttpResponse("<script>alert('Invalid username or password');window.location='/login'</script>")
 
 
-
 class ViewUser(View):
     def get(self,request):
-        return render(request,'viewuser.html')    
+        obj = UserTable.objects.all()
+        return render(request,'viewuser.html', {'val': obj})    
+
+class AcceptUser(View):
+    def get(self,request, id):
+        obj = LoginTable.objects.get(id=id)
+        obj.Type="user"
+        obj.save()
+        return redirect('ViewUser')
+    
+class RejectUser(View):
+    def get(self,request, id):
+        obj = LoginTable.objects.get(id=id)
+        obj.Type="rejected"
+        obj.save()
+        return redirect('ViewUser')
 
 class Complaint(View):
     def get(self,request):
-        return render(request,'complaint&reply.html')
+        obj = ComplaintTable.objects.all()
+        return render(request,'complaint&reply.html',{'val': obj})
     
 class Reply(View):
     def get(self,request):
@@ -39,5 +50,9 @@ class Reply(View):
     
 class Feedback(View):
     def get(self,request):
-        return render(request,'feedback.html')
+        obj = FeedbackTable.objects.all()
+        return render(request,'feedback.html',{'val': obj})
     
+class AdminHome(View):
+    def get(self,request):
+        return render(request,'home.html')
